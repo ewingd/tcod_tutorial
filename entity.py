0d6cde4b80bc78e18fe321622ff14c6459e5f58a
David Ewing <david@ewingsoftware.com>
2020-07-11 10:59:01 -0400

    Pre-6 Refactoring

--- entity.py
+++ entity.py
@@ -14,8 +14,11 @@ class Entity:
     A generic object ro represent players, enemies, items, etc.
     """
 
+    gamemap: GameMap
+
     def __init__(
         self,
+        gamemap: Optional[GameMap] = None,
         x: int = 0,
         y: int = 0,
         char: str = "?",
@@ -29,6 +32,10 @@ class Entity:
         self.color = color
         self.name = name
         self.blocks_movement = blocks_movement
+        if gamemap:
+            # If gamemap isn't provided now then it will be set later
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def spawn(self: T, gamemap: GameMap, x: int, y: int) -> T:
         """
@@ -37,8 +44,21 @@ class Entity:
         clone = copy.deepcopy(self)
         clone.x = x
         clone.y = y
+        clone.gamemap = gamemap
         gamemap.entities.add(clone)
         return clone
+
+    def place(self, x: int, y: int, gamemap: Optional[GameMap] = None) -> None:
+        """
+        Place this entity at a new location.  Handles moving across GameMaps.
+        """
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "gamemap"):  # Possibly ininitialized.
+                self.gamemap.entities.remove(self)
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def move(self, dx: int, dy: int) -> None:
         self.x += dx
--- input_handlers.py
+++ input_handlers.py
@@ -1,28 +1,50 @@
-from typing import Optional
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
 
 import tcod.event
 
-from actions import Action, EscapeAction, BumpAction
+from actions import Action, BumpAction, EscapeAction
+
+if TYPE_CHECKING:
+    from engine import Engine
 
 
 class EventHandler(tcod.event.EventDispatch[Action]):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+            self.engine.handle_enemy_turns()
+            # Update the FOV before the players next action
+            self.engine.update_fov()
+
     def ev_quit(self, event: tcod.event.Quit):
         raise SystemExit()
 
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
         key = event.sym
+        player = self.engine.player
 
         if key in [tcod.event.K_UP, tcod.event.K_w]:
-            action = BumpAction(dx=0, dy=-1)
+            action = BumpAction(player, dx=0, dy=-1)
         elif key in [tcod.event.K_DOWN, tcod.event.K_s]:
-            action = BumpAction(dx=0, dy=1)
+            action = BumpAction(player, dx=0, dy=1)
         elif key in [tcod.event.K_LEFT, tcod.event.K_a]:
-            action = BumpAction(dx=-1, dy=0)
+            action = BumpAction(player, dx=-1, dy=0)
         elif key in [tcod.event.K_RIGHT, tcod.event.K_d]:
-            action = BumpAction(dx=1, dy=0)
+            action = BumpAction(player, dx=1, dy=0)
 
         elif key == tcod.event.K_ESCAPE:
-            action = EscapeAction()
+            action = EscapeAction(player)
 
         return action
