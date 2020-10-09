3b8a189eac4135c272125ce9fad104d23d84fa4f
David Ewing <david@ewingsoftware.com>
2020-10-08 22:05:43 -0400

    Added support for saving and loading games. http://rogueliketutorials.com/tutorials/tcod/v2/part-10/

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ import input_handlers
 import setup_game
 
 
+def save_game(handler: input_handlers.BaseEventHandler, filename: str) -> None:
+    """If the current event handler has an active Engine, then save it."""
+    if isinstance(handler, input_handlers.EventHandler):
+        handler.engine.save_as(filename)
+        print("Game saved.")
+
+
 def main():
     screen_width = 80
     screen_height = 50
@@ -47,10 +54,10 @@ def main():
         except exceptions.QuitWithoutSaving:
             raise
         except SystemExit:  # Save and quit
-            # TODO: Add the save function here
+            save_game(handler, "savegame.sav")
             raise
         except BaseException:  # Save on any other unexpected exception
-            # TODO: Add the save function here
+            save_game(handler, "savegame.sav")
             raise
 
 
